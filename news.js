4c18ccdbb2f4825900c8eadccb578e0935b2887f
dante785412 <gdigangiibb@web.de>
2020-04-21 14:55:20 +0200

    first version

--- news.js
+++ news.js
@@ -8,6 +8,8 @@ var request = require('request');
 //variablen
 var parameter =process.argv[2];
 var category = process.argv.length > 2 ? process.argv[2] : 'business';
+
+
 request('https://newsapi.org/v2/top-headlines?country=de&apiKey=' + NEWS_API_KEY + '&category=' + category, function (error, response, body){
     
     if (response.statusCode === 200) {
@@ -18,6 +20,8 @@ request('https://newsapi.org/v2/top-headlines?country=de&apiKey=' + NEWS_API_KEY
     for(var i = 0; i < 5 ; i++) { //statt 5 stand <bodyObj.articles.length>
         console.log((i+1) + ', ' + bodyObj.articles[i].title);
     }
+
+
 });
 
 
